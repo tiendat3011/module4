a782f8720eb422494def907fd6114744cbd77a66
tiendat3011 <tiendat30111999@gmail.com>
2022-07-27 20:20:15 +0700

    up2

---
+++  ss10_session_cookie/exercise/cart/src/main/java/com/example/cart/controller/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.cart.controller;
+
+import com.example.cart.model.Cart;
+import com.example.cart.model.Product;
+import com.example.cart.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@SessionAttributes("cart")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @ModelAttribute("cart")
+    public Cart productInCart() {
+        return new Cart();
+    }
+
+
+    @GetMapping("{id}/detail")
+    public String samsung(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "detail-product";
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        model.addAttribute("productList", productService.findAll());
+        return "home";
+    }
+
+    @GetMapping("{id}/add-cart")
+    public String addToCart(@PathVariable int id,
+                            @ModelAttribute("cart") Cart cart,
+                            @RequestParam("action") String action) {
+        Product product = productService.findById(id);
+
+        if (action.equals("show")) {
+            cart.addProduct(product);
+            return "redirect:/cart";
+        }
+        cart.addProduct(product);
+        return "redirect:/list";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteFromCart(@PathVariable Integer id, @SessionAttribute Cart cart) {
+        Product product = productService.findById(id);
+        if (product == null) {
+            return "redirect:/list";
+        } else {
+            cart.deleteProduct(product);
+            return "redirect:/cart";
+        }
+    }
+}
---
+++  ss10_session_cookie/exercise/cart/src/main/java/com/example/cart/service/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.cart.service;
+
+
+import com.example.cart.model.Product;
+import com.example.cart.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product findById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
