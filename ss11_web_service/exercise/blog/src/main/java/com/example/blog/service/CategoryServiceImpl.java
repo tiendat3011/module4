2a042d136c9ef91ef22d23d3989d9d8a0fcbad52
tiendat3011 <tiendat30111999@gmail.com>
2022-07-28 18:57:57 +0700

    up2

---
+++ ss11_web_service/exercise/blog/src/main/java/com/example/blog/service/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.blog.service;
+
+import com.example.blog.model.Category;
+import com.example.blog.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void create(String typeBlog) {
+        categoryRepository.create(typeBlog);
+    }
+}
